91046ec92157a17402031e01f7822b13763e82ad
raabe <raabedominik13@gmailc.com>
2026-03-17 11:55:47 +0100

    ProjectStart - project inicialization

---
+++ src/main/java/cz/fei/equipmentrental/EquipmentRentalApplication.java
@@ -0,0 +1,13 @@
+package cz.fei.equipmentrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/fei/equipmentrental/EquipmentRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.fei.equipmentrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
