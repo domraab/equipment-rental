297d07872afda6d7663528d387fd8ba3fd129da7
raabe <raabedominik13@gmailc.com>
2026-03-17 13:23:56 +0100

    fix - resolve PMD  violation in LateFeeCalculator

--- src/main/java/cz/fei/equipmentrental/domain/LateFeeCalculator.java
+++ src/main/java/cz/fei/equipmentrental/domain/LateFeeCalculator.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class LateFeeCalculator {
     public BigDecimal calculate(int daysLate, BigDecimal dailyRate) {
 
-        BigDecimal penaltyRate = dailyRate.multiply(new BigDecimal("2"));
+        BigDecimal penaltyRate = dailyRate.multiply(BigDecimal.valueOf(2));
 
         return penaltyRate.multiply(new BigDecimal(daysLate));
     }
