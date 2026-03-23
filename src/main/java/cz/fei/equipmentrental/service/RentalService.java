6ac2321d62c3d9b26bd2cf24d1287fac90c3ca78
raabe <raabedominik13@gmailc.com>
2026-03-23 16:17:40 +0100

    test - implement date validation to pass test

--- src/main/java/cz/fei/equipmentrental/service/RentalService.java
+++ src/main/java/cz/fei/equipmentrental/service/RentalService.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 public class RentalService {
     public void createRental(Long userId, Long equipmnetID, LocalDate startDate, LocalDate endDate){
-
+        if (endDate.isBefore(startDate)) {
+            throw new IllegalArgumentException("Datum konce vypůjčení nesmí být před začátkem");
+        }
     }
 }
